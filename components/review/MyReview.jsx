78baab89d237123afedb3ee09dfadd4c947401f1
ehfdl <cmaw@naver.com>
2023-01-09 12:59:29 +0900

    🔧 Add(Review):
    폴더 update

---
+++ components/review/MyReview.jsx
@@ -0,0 +1,170 @@
+import { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  Text,
+  useColorScheme,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import styled from "@emotion/native";
+import { authService, dbService } from "../../firebase";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import Vote from "../components/Vote";
+import { useFocusEffect } from "@react-navigation/native";
+import { GREEN_COLOR, YELLOW_COLOR } from "../../colors";
+import { signOut } from "firebase/auth";
+import { SCREEN_WIDTH } from "../utils";
+
+const Container = styled.ScrollView`
+  padding: 20px;
+`;
+const ReviewWrapper = styled.TouchableOpacity``;
+const ContentsBox = styled.View`
+  padding: 10px 30px;
+`;
+const SeparateLine = styled.View`
+  width: ${SCREEN_WIDTH / 1.1 + "px"};
+  height: 1px;
+  background-color: lightgray;
+  margin-top: 10px;
+`;
+const ListTitle = styled.Text`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${(props) => props.theme.color.listTitle};
+`;
+const Title = styled.Text`
+  color: ${(props) => props.theme.color.title};
+  font-size: 20px;
+`;
+
+const Contents = styled.Text`
+  color: ${(props) => props.theme.color.title};
+  font-size: 17px;
+  margin-top: 5px;
+  margin-left: 2px;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 10px;
+`;
+
+const ReviewAt = styled.Text`
+  font-size: 14px;
+  color: ${(props) => props.theme.color.title};
+`;
+const VSeperator = styled.View`
+  height: 10px;
+`;
+
+export default function MyReview({
+  navigation: { navigate, reset, setOptions },
+}) {
+  const isDark = useColorScheme() === "dark";
+  const [reviews, setReviews] = useState([]);
+
+  const goToReview = (theReview) => {
+    navigate("Stack", {
+      screen: "Review",
+      params: { review: theReview, from: "My" },
+    });
+  };
+
+  const logout = () => {
+    signOut(authService)
+      .then(() => {
+        console.log("로그아웃 성공");
+        navigate("Main");
+      })
+      .catch((err) => alert(err));
+  };
+  useFocusEffect(
+    useCallback(() => {
+      if (!authService.currentUser) {
+        // 비로그인 상태에서 마이페이지 접근 시 로그인화면으로 이동하고, 뒤로가기 시 무비탭
+        reset({
+          index: 1,
+          routes: [
+            {
+              name: "Tabs",
+              params: {
+                screen: "Main",
+              },
+            },
+            {
+              name: "Stack",
+              params: {
+                screen: "Login",
+              },
+            },
+          ],
+        });
+        return;
+      }
+
+      setOptions({
+        headerRight: () => {
+          return (
+            <TouchableOpacity style={{ marginRight: 10 }} onPress={logout}>
+              <Text style={{ color: isDark ? YELLOW_COLOR : GREEN_COLOR }}>
+                로그아웃
+              </Text>
+            </TouchableOpacity>
+          );
+        },
+      });
+
+      const q = query(
+        collection(dbService, "reviews"),
+        orderBy("createdAt", "desc"),
+        where("userId", "==", authService.currentUser?.uid)
+      );
+      const unsubcribe = onSnapshot(q, (snapshot) => {
+        const newReviews = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setReviews(newReviews);
+      });
+      return unsubcribe;
+    }, [])
+  );
+
+  return (
+    <FlatList
+      contentContainerStyle={{ padding: 20 }}
+      data={reviews}
+      ItemSeparatorComponent={VSeperator}
+      ListHeaderComponent={
+        <ListTitle style={{ marginBottom: 20 }}>내가 쓴 글</ListTitle>
+      }
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <ReviewWrapper onPress={() => goToReview(item)}>
+          <ContentsBox>
+            <Title numberOfLines={1}>{item.title}</Title>
+            <Contents numberOfLines={1}>{item.contents}</Contents>
+            <Row>
+              <Vote vote_average={item.rating} />
+              <ReviewAt>
+                {new Date(item.createdAt).toLocaleDateString("kr")}
+              </ReviewAt>
+            </Row>
+          </ContentsBox>
+          <SeparateLine />
+        </ReviewWrapper>
+      )}
+    />
+  );
+}
---
+++ components/review/ReviewModal.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import styled from "@emotion/native";
+import { addDoc, collection } from "firebase/firestore";
+import { Modal, Text } from "react-native";
+import { Rating } from "react-native-ratings";
+import { authService, dbService } from "../firebase";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const TitleInput = styled.TextInput`
+  padding: 10px;
+  background-color: #fffcfd;
+  border-radius: 10px;
+`;
+const ContentInput = styled(TitleInput)`
+  min-height: 100px;
+`;
+const RegisterBtn = styled.TouchableOpacity`
+  border-radius: 50px;
+  width: 100%;
+  height: 50px;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+`;
+const CancelBtn = styled.TouchableOpacity`
+  align-items: flex-end;
+`;
+const InputWrapper = styled.View``;
+
+const Backdrop = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Dialog = styled.KeyboardAvoidingView`
+  background-color: ${(props) => props.theme.color.modalBg};
+  width: 80%;
+  height: 70%;
+  padding: 20px;
+  justify-content: space-between;
+  border-radius: 20px;
+`;
+const ModalTitle = styled.Text`
+  font-size: 20px;
+  font-weight: 600;
+  color: white;
+  margin-bottom: 10px;
+  margin-top: 10px;
+`;
+
+export default function ReviewModal({
+  isOpenModal,
+  setIsOpenModal /* movieId */,
+}) {
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalContent, setModalContent] = useState("");
+  const [ratings, setRatings] = useState(0);
+  const getRatings = (rating) => {
+    setRatings(rating);
+  };
+  const addReview = async () => {
+    await addDoc(collection(dbService, "reviews"), {
+      title: modalTitle,
+      contents: modalContent,
+      createdAt: Date.now(),
+      rating: ratings,
+      userId: authService.currentUser?.uid,
+      // movieId,
+    });
+    setIsOpenModal(false);
+    setModalTitle("");
+    setModalContent("");
+    setRatings(0);
+  };
+  return (
+    <Modal visible={isOpenModal} transparent animationType="slide">
+      <Backdrop>
+        <Dialog>
+          <InputWrapper>
+            <CancelBtn onPress={() => setIsOpenModal(false)}>
+              <MaterialIcons name="cancel" size={24} color="red" />
+            </CancelBtn>
+            <ModalTitle>별점</ModalTitle>
+            <Rating
+              startingValue={0}
+              style={{
+                alignItems: "flex-start",
+              }}
+              onFinishRating={getRatings}
+              ratingCount={5}
+              imageSize={20}
+              tintColor="black"
+            />
+            <ModalTitle>제목</ModalTitle>
+            <TitleInput
+              value={modalTitle}
+              onChangeText={(text) => setModalTitle(text)}
+            />
+            <ModalTitle>내용</ModalTitle>
+            <ContentInput
+              textAlignVertical="top"
+              value={modalContent}
+              onChangeText={(text) => setModalContent(text)}
+              multiline
+              maxLength={300}
+            />
+          </InputWrapper>
+          <RegisterBtn
+            disabled={!ratings || !modalTitle || !modalContent}
+            onPress={addReview}
+            activeOpacity={0.8}
+          >
+            <Text>등록하기</Text>
+          </RegisterBtn>
+        </Dialog>
+      </Backdrop>
+    </Modal>
+  );
+}
