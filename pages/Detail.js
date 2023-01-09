442354092184eac2b37a33639a14c27fae642d2f
chha5232 <chha5232@gmail.com>
2023-01-09 13:57:27 +0900

    z

--- pages/Detail.js
+++ pages/Detail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 import Loader from "../components/Loader";
-
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { authService, dbService } from "../firebase";
 import ReviewModal from "../components/ReviewModal";
