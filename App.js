905e4dcf7f30a007ef173234e4cfcb5c76df5476
tyoung <taeyoung917@gmail.com>
2023-01-12 12:03:02 +0900

    💄 font twayair 추가
    @young-02

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import {
   DarkTheme,
   DefaultTheme,
   NavigationContainer,
-} from "@react-navigation/native";
-import Root from "./navigation/Root";
-import { useColorScheme } from "react-native";
-import { ThemeProvider } from "@emotion/react";
-import { darkTheme, lightTheme } from "./theme";
-
-import { QueryClient, QueryClientProvider } from "react-query";
-
+} from '@react-navigation/native';
+import Root from './navigation/Root';
+import { useColorScheme } from 'react-native';
+import { ThemeProvider } from '@emotion/react';
+import { darkTheme, lightTheme } from './theme';
+import * as Font from 'expo-font';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  const isDark = useColorScheme() === "dark";
+  // const [isReady, setIsReady] = useEffect(false);
+
+  // useEffect(async () => {
+  //   await Font.loadAsync({
+  //     twayair: require('./assets/fonts/tway_air.ttf'),
+  //   });
+  //   setIsReady(true);
+  // }, []);
+
+  Font.loadAsync({
+    twayair: require('./assets/fonts/tway_air.ttf'),
+  });
+
+  const isDark = useColorScheme() === 'dark';
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
