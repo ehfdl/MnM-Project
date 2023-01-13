c85e464ee38c0f89fc4c14bb71644710c3bbf3f1
tyoung <taeyoung917@gmail.com>
2023-01-13 12:40:04 +0900

    💄 콘솔 지움

--- pages/Main.js
+++ pages/Main.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import { ActivityIndicator, FlatList, ScrollView, View } from "react-native";
-import styled from "@emotion/native";
-import Loader from "../components/review/Loader";
-import Swiper from "react-native-swiper";
-import Slide from "../components/Main/Slide";
-import VCard from "../components/Main/VCard";
-import HCard from "../components/Main/HCard";
-import { useQuery, useQueryClient } from "react-query";
-import { getEventList } from "../api";
+import React, { useState } from 'react';
+import { ActivityIndicator, FlatList, ScrollView, View } from 'react-native';
+import styled from '@emotion/native';
+import Loader from '../components/review/Loader';
+import Swiper from 'react-native-swiper';
+import Slide from '../components/Main/Slide';
+import VCard from '../components/Main/VCard';
+import HCard from '../components/Main/HCard';
+import { useQuery, useQueryClient } from 'react-query';
+import { getEventList } from '../api';
 
 export default function Main({ navigation: { navigate } }) {
   const [isRefreshing, setIsRefreshing] = useState(false);
-  const [cate, setCate] = useState("연극");
+  const [cate, setCate] = useState('연극');
   const queryClient = useQueryClient();
 
   const { data: getEventListsData, isLoading: isLoadingEL } = useQuery(
-    ["Mains", "getEventLists"],
+    ['Mains', 'getEventLists'],
     getEventList
   );
 
   const onRefresh = async () => {
     setIsRefreshing(true);
-    await queryClient.refetchQueries(["Mains"]);
+    await queryClient.refetchQueries(['Mains']);
     setIsRefreshing(false);
   };
 
@@ -30,8 +30,8 @@ export default function Main({ navigation: { navigate } }) {
   };
 
   const imgId = (id) => {
-    id = id.split("atchFileId=");
-    id = id[1].split("&");
+    id = id.split('atchFileId=');
+    id = id[1].split('&');
     return id[0];
   };
 
@@ -46,11 +46,11 @@ export default function Main({ navigation: { navigate } }) {
   }
   // 날짜 정렬
   function leadingZeros(n, digits) {
-    var zero = "";
+    var zero = '';
     n = n.toString();
 
     if (n.length < digits) {
-      for (i = 0; i < digits - n.length; i++) zero += "0";
+      for (i = 0; i < digits - n.length; i++) zero += '0';
     }
     return zero + n;
   }
@@ -62,16 +62,15 @@ export default function Main({ navigation: { navigate } }) {
   const sorting = () => {
     now =
       leadingZeros(now.getFullYear(), 4) +
-      "-" +
+      '-' +
       leadingZeros(now.getMonth() + 1, 2) +
-      "-" +
+      '-' +
       leadingZeros(now.getDate(), 2);
 
     const res_date = [...getEventListsData.culturalEventInfo.row].filter(
       (item) => {
-        item.END_DATE = item.END_DATE.split(" ")[0];
+        item.END_DATE = item.END_DATE.split(' ')[0];
         if (item.END_DATE > now) {
-          console.log(item.END_DATE);
           return item;
         }
       }
@@ -102,47 +101,47 @@ export default function Main({ navigation: { navigate } }) {
             ))}
           </Swiper>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <Toggle onPress={() => onPressCate("연극")}>
+            <Toggle onPress={() => onPressCate('연극')}>
               <ListTitle title="연극" cate={cate}>
                 연극
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("뮤지컬/오페라")}>
+            <Toggle onPress={() => onPressCate('뮤지컬/오페라')}>
               <ListTitle title="뮤지컬/오페라" cate={cate}>
                 뮤지컬/오페라
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("국악")}>
+            <Toggle onPress={() => onPressCate('국악')}>
               <ListTitle title="국악" cate={cate}>
                 국악
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("클래식")}>
+            <Toggle onPress={() => onPressCate('클래식')}>
               <ListTitle title="클래식" cate={cate}>
                 클래식
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("축제-전통/역사")}>
+            <Toggle onPress={() => onPressCate('축제-전통/역사')}>
               <ListTitle title="축제-전통/역사" cate={cate}>
                 축제-전통/역사
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("무용")}>
+            <Toggle onPress={() => onPressCate('무용')}>
               <ListTitle title="무용" cate={cate}>
                 무용
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("콘서트")}>
+            <Toggle onPress={() => onPressCate('콘서트')}>
               <ListTitle title="콘서트" cate={cate}>
                 콘서트
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("전시/미술")}>
+            <Toggle onPress={() => onPressCate('전시/미술')}>
               <ListTitle title="전시/미술" cate={cate}>
                 전시/미술
               </ListTitle>
             </Toggle>
-            <Toggle onPress={() => onPressCate("기타")}>
+            <Toggle onPress={() => onPressCate('기타')}>
               <ListTitle title="기타" cate={cate}>
                 기타
               </ListTitle>
@@ -181,21 +180,21 @@ export default function Main({ navigation: { navigate } }) {
         />
       )}
       ItemSeparatorComponent={
-        <View style={{ height: 15, flexDirection: "row" }} />
+        <View style={{ height: 15, flexDirection: 'row' }} />
       }
     />
   );
 }
 
 const ListTitle = styled.Text`
-  font-family: "twayair";
+  font-family: 'twayair';
   margin-top: 20px;
   margin-bottom: 20px;
   margin-left: 20px;
   font-size: 20px;
   font-weight: 500;
   color: ${(props) =>
-    props.cate === props.title ? "#e50015" : props.theme.text};
+    props.cate === props.title ? '#e50015' : props.theme.text};
 `;
 
 const Toggle = styled.TouchableOpacity`
